40cc18f9a2beb0a43b7ef609ad624d7de536296c
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2017-11-16 19:40:32 +0000

    made changes to project

--- Xylophone Fun/Xylophone Fun/ViewController.swift
+++ Xylophone Fun/Xylophone Fun/ViewController.swift
@@ -13,14 +13,10 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     
     var audioPlayer: AVAudioPlayer!
     
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 
     @IBAction func notePressed(_ sender: UIButton) {
         
